52b3369aec155e14be79ea4e635e35835dcf7c3c
lsiwh37249 <lsiwh37249@gmail.com>
2024-09-10 18:51:58 +0900

    add LB

--- src/fishregression/cli.py
+++ src/fishregression/cli.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import requests
+import typer
+
 
 app = FastAPI()
 
@@ -13,7 +15,10 @@ def lr_api(l):
         'length': l,
     }
 
-    response = requests.get('http://localhost:8001/get_weight', params=params, headers=headers)    
+#    response = requests.get('http://ec2-43-202-46-68.ap-northeast-2.compute.amazonaws.com:8080/get_weight', params=params, headers=headers)    
+    
+    response = requests.get('http://localhost:8002/get_weight', params=params, headers=headers)    
+    
     j = response.json()
     r = j.get("weight")
     return r
@@ -29,7 +34,8 @@ def knn_api(l,w,n):
         'neighbor': n,
     }
 
-    response = requests.get('http://localhost:8002/fish', params=params, headers=headers)
+#    response = requests.get('http://ec2-43-202-46-68.ap-northeast-2.compute.amazonaws.com:8080/fish', params=params, headers=headers)
+    response = requests.get('http://localhost:8003/fish', params=params, headers=headers)
     j = response.json()
     r = j.get("prediction")
     return r
@@ -38,9 +44,9 @@ def knn_api(l,w,n):
 def read_root():
     return {"Hello": "world"}
 
-@app.get("/predict")
-def predict():
-    length =3
+def to_predict(
+        length: float = typer.Option(show_default=False)
+        ):
 
     neighbor = 5
 
@@ -51,3 +57,6 @@ def predict():
     fish_class = knn_api(length, weight, neighbor)
 
     print(f"length:{length} 물고기는 weight:{weight} 으로 예측되며 종류는 {fish_class}입니다")
+
+def predict():
+    typer.run(to_predict)
